1f26114e05a9c5d3420f79588a6e725f979364da
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-01-11 07:49:35 +0100

    Added Directions table

--- Math/Utility/CubeUtility.cs
+++ Math/Utility/CubeUtility.cs
@@ -29,6 +29,16 @@ namespace Common
 			new int[] { 1, 5, 6, 1, 6, 2, -1 }
 		};
 
+		public static readonly Vector3[] DIRECTIONS = new Vector3[]
+		{
+			Vector3.back,
+			Vector3.forward,
+			Vector3.left,
+			Vector3.right,
+			Vector3.down,
+			Vector3.up
+		};
+
 		public enum Direction
 		{
 			Back,
