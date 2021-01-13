d9975ed65f3ae9ca0c40542f860706ddafe9055b
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-01-13 06:40:59 +0100

    Added Range structs

---
+++ Math/Range.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Common
+{
+	[Serializable]
+	public struct Range : IEquatable<Range>
+	{
+		public float min;
+		public float max;
+
+		public static readonly Range Max = new Range(float.MinValue, float.MaxValue);
+
+		public Range(float min, float max)
+		{
+			this.min = min;
+			this.max = max;
+		}
+
+		public float Length
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get { return max - min; }
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(float f)
+		{
+			return min <= f && f <= max;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(Range other)
+		{
+			return min <= other.min && other.max <= max;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Overlaps(Range other)
+		{
+			return min <= other.max && other.min <= max;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Range Intersection(Range other)
+		{
+			return new Range(
+				Math.Max(min, other.min),
+				Math.Min(max, other.max)
+			);
+		}
+
+		public bool Equals(Range other)
+		{
+			return (
+				Mathx.AreEqual(min, other.min) &&
+				Mathx.AreEqual(max, other.max)
+			);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is Range && Equals((Range)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return min.GetHashCode() ^ (max.GetHashCode() << 2);
+		}
+
+		public override string ToString()
+		{
+			return string.Format("Range({0}, {1})", min, max);
+		}
+	}
+}
---
+++ Math/RangeInt.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Common
+{
+	[Serializable]
+	public struct RangeInt : IEquatable<RangeInt>
+	{
+		public int min;
+		public int max;
+
+		public static readonly RangeInt Max = new RangeInt(int.MinValue, int.MaxValue);
+
+		public RangeInt(int min, int max)
+		{
+			this.min = min;
+			this.max = max;
+		}
+
+		public int Length
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get { return max - min; }
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(int i)
+		{
+			return min <= i && i <= max;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(RangeInt other)
+		{
+			return min <= other.min && other.max <= max;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Overlaps(RangeInt other)
+		{
+			return min <= other.max && other.min <= max;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public RangeInt Intersection(RangeInt other)
+		{
+			return new RangeInt(
+				Math.Max(min, other.min),
+				Math.Min(max, other.max)
+			);
+		}
+
+		public bool Equals(RangeInt other)
+		{
+			return (
+				min == other.min &&
+				max == other.max
+			);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is RangeInt && Equals((RangeInt)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return min.GetHashCode() ^ (max.GetHashCode() << 2);
+		}
+
+		public override string ToString()
+		{
+			return string.Format("RangeInt({0}, {1})", min, max);
+		}
+	}
+}
