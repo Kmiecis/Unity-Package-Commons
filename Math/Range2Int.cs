2ea9229e9ebd22a63e0194103280b3e5cb55c938
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-01-14 08:44:36 +0100

    Created Range2Int

---
+++ Math/Range2Int.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Common
+{
+	[Serializable]
+	public struct Range2Int : IEquatable<Range2Int>
+	{
+		public Vector2Int min;
+		public Vector2Int max;
+
+		public static readonly Range2Int Max = new Range2Int(Vector2Int.one * int.MinValue, Vector2Int.one * int.MaxValue);
+
+		public Range2Int(Vector2Int min, Vector2Int max)
+		{
+			this.min = min;
+			this.max = max;
+		}
+
+		public Vector2Int Size
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get { return max - min; }
+		}
+		
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(Vector2Int v)
+		{
+			return (
+				Mathx.IsLesserOrEqual(min, v).All() &&
+				Mathx.IsLesserOrEqual(v, max).All()
+			);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(Range2Int other)
+		{
+			return (
+				Mathx.IsLesserOrEqual(min, other.min).All() &&
+				Mathx.IsLesserOrEqual(other.max, max).All()
+			);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Overlaps(Range2Int other)
+		{
+			return (
+				Mathx.IsLesserOrEqual(min, other.max).All() &&
+				Mathx.IsLesserOrEqual(other.min, max).All()
+			);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Range2Int Intersection(Range2Int other)
+		{
+			return new Range2Int(
+				Mathx.Max(min, other.min),
+				Mathx.Min(max, other.max)
+			);
+		}
+
+		public bool Equals(Range2Int other)
+		{
+			return (
+				Mathx.AreEqual(min, other.min) &&
+				Mathx.AreEqual(max, other.max)
+			);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is Range2Int && Equals((Range2Int)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return min.GetHashCode() ^ (max.GetHashCode() << 2);
+		}
+
+		public override string ToString()
+		{
+			return string.Format("Range2Int({0}, {1})", min, max);
+		}
+	}
+}
