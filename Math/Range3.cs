592b6814f2dd40d717c00f076d39ca8638b9e64d
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-01-15 05:52:28 +0100

    Range3

---
+++ Math/Range3.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Common
+{
+	[Serializable]
+	public struct Range3 : IEquatable<Range3>
+	{
+		public Vector3 min;
+		public Vector3 max;
+
+		public static readonly Range3 Max = new Range3(
+			Vector3.one * float.MinValue,
+			Vector3.one * float.MaxValue
+		);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Range3(Vector3 min, Vector3 max)
+		{
+			this.min = min;
+			this.max = max;
+		}
+
+		public Vector3 Extents
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get { return max - min; }
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(Vector3 v)
+		{
+			return (
+				Mathx.AreLesserOrEqual(min, v) &&
+				Mathx.AreLesserOrEqual(v, max)
+			);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Contains(Range3 other)
+		{
+			return (
+				Mathx.AreLesserOrEqual(min, other.min) &&
+				Mathx.AreLesserOrEqual(other.max, max)
+			);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool Overlaps(Range3 other)
+		{
+			return (
+				Mathx.AreLesserOrEqual(min, other.max) &&
+				Mathx.AreLesserOrEqual(other.min, max)
+			);
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Range3 Intersection(Range3 other)
+		{
+			return new Range3(
+				Mathx.Max(min, other.min),
+				Mathx.Min(max, other.max)
+			);
+		}
+
+		public bool Equals(Range3 other)
+		{
+			return (
+				Mathx.AreEqual(min, other.min) &&
+				Mathx.AreEqual(max, other.max)
+			);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is Range3 && Equals((Range3)obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return min.GetHashCode() ^ (max.GetHashCode() << 2);
+		}
+
+		public override string ToString()
+		{
+			return string.Format("Range3({0}, {1})", min, max);
+		}
+	}
+}
