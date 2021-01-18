2c132a1f88593379aebbe70bf43c416feb03270a
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-01-18 05:37:51 +0100

    Added ColorUtility

---
+++ Utilities/ColorUtility.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Common
+{
+	public static class ColorUtility
+	{
+		public static readonly Color32 CLEAR = Color.clear;
+
+		public static readonly Color32 WHITE = Color.white;
+		public static readonly Color32 BLACK = Color.black;
+
+		public static readonly Color32 RED = Color.red;
+		public static readonly Color32 GREEN = Color.green;
+		public static readonly Color32 BLUE = Color.blue;
+	}
+}
