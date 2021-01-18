b5ec9dd6b6593faaf719f1c4b055b7125fef2122
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-01-18 10:17:23 +0100

    Tidy up in CubeUtility

--- Math/Utility/CubeUtility.cs
+++ Math/Utility/CubeUtility.cs
@@ -4,9 +4,9 @@ namespace Common
 {
 	public static class CubeUtility
 	{
-		public const int VCOUNT = 8;
 		public const float CENTER_TO_SIDE = 0.5f;
 
+		public const int VCOUNT = 8;
 		public static readonly Vector3[] VERTICES = new Vector3[]
 		{
 			new Vector3(-CENTER_TO_SIDE, -CENTER_TO_SIDE, -CENTER_TO_SIDE),
@@ -18,6 +18,17 @@ namespace Common
 			new Vector3(+CENTER_TO_SIDE, +CENTER_TO_SIDE, +CENTER_TO_SIDE),
 			new Vector3(+CENTER_TO_SIDE, -CENTER_TO_SIDE, +CENTER_TO_SIDE)
 		};
+		
+		public const int DCOUNT = 6;
+		public static readonly Vector3Int[] DIRECTIONS = new Vector3Int[]
+		{
+			Vector3Int.back,
+			Vector3Int.forward,
+			Vector3Int.left,
+			Vector3Int.right,
+			Vector3Int.down,
+			Vector3Int.up
+		};
 
 		public static readonly int[][] TRIANGLES = new int[][]
 		{
@@ -27,16 +38,6 @@ namespace Common
 			new int[] { 3, 2, 6, 3, 6, 7, -1 },
 			new int[] { 4, 0, 3, 4, 3, 7, -1 },
 			new int[] { 1, 5, 6, 1, 6, 2, -1 }
-		};
-
-		public static readonly Vector3Int[] DIRECTIONS = new Vector3Int[]
-		{
-			Vector3Int.back,
-			Vector3Int.forward,
-			Vector3Int.left,
-			Vector3Int.right,
-			Vector3Int.down,
-			Vector3Int.up
 		};
 
 		public enum Direction
