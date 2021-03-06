f396256daaa6e26902fd18ec5c370262b9b7dd3f
Sławomir Kmiecik <33656921+Kmiecis@users.noreply.github.com>
2021-03-06 09:49:22 +0100

    Added InvokeOverTime

--- Utilities/CoroutineUtility.cs
+++ Utilities/CoroutineUtility.cs
@@ -6,24 +6,43 @@ namespace Common
 {
 	public static class CoroutineUtility
 	{
-		public static IEnumerator CreateInvokeNextFrame(Action callback)
+		public static IEnumerator InvokeNextFrame(Action callback)
 		{
 			yield return null;
 			callback.Invoke();
 		}
 
-		public static IEnumerator CreateInvokeDelayed(Action callback, float delay)
+		public static IEnumerator InvokeDelayed(Action callback, float delay, bool unscaled = false)
 		{
-			yield return new WaitForSecondsRealtime(delay);
+			if (unscaled)
+				yield return new WaitForSecondsRealtime(delay);
+			else
+				yield return new WaitForSeconds(delay);
 			callback.Invoke();
 		}
 
-		public static IEnumerator CreateInvokeRepeating(Action callback, float delay)
+		public static IEnumerator InvokeRepeating(Action callback, float delay)
 		{
 			while (true)
 			{
-				yield return CreateInvokeDelayed(callback, delay);
+				yield return InvokeDelayed(callback, delay);
 			}
+		}
+
+		public static IEnumerator InvokeOverTime(Action<float> consumer, float duration, bool unscaled = false)
+		{
+			float t = 0.0f;
+
+			while (t < duration)
+			{
+				consumer(t);
+
+				t += unscaled ? Time.unscaledDeltaTime : Time.deltaTime;
+
+				yield return null;
+			}
+
+			consumer(1.0f);
 		}
 	}
 }
