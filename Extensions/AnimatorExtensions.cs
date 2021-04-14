3641855247458b4d2b6a509bdb09a8c7622dbe37
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-04-14 16:05:39 +0200

    Added AnimatorExtensions

---
+++ Extensions/AnimatorExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public static class AnimatorExtensions
+    {
+        public static void SetTrigger(this Animator self, int id, bool active)
+        {
+            if (active)
+                self.SetTrigger(id);
+            else
+                self.ResetTrigger(id);
+        }
+    }
+}
