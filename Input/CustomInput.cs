c6f98b5f2ad0c37b68c38c1a0a7590a68438cebd
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-11-07 09:57:32 +0100

    Added Property editor

--- Input/CustomInput.cs
+++ Input/CustomInput.cs
@@ -3,152 +3,152 @@ using UnityEngine;
 
 namespace Common
 {
-    public class CustomInput
+    public static class CustomInput
     {
         private const string HORIZONTAL = "Horizontal";
         private const string VERTICAL = "Vertical";
         private const int HORIZONTAL_ID = 0;
         private const int VERTICAL_ID = 1;
 
-        private Dictionary<KeyCode, VirtualButton> m_Keys = new Dictionary<KeyCode, VirtualButton>();
-        private Dictionary<string, VirtualButton> m_Buttons = new Dictionary<string, VirtualButton>();
+        private static readonly Dictionary<KeyCode, VirtualButton> m_Keys = new Dictionary<KeyCode, VirtualButton>();
+        private static readonly Dictionary<string, VirtualButton> m_Buttons = new Dictionary<string, VirtualButton>();
 
-        private Vector2 m_Axes;
-        private Vector3 m_MousePosition;
+        private static Vector2 m_Axes;
+        private static Vector3 m_MousePosition;
 
-        public float GetAxisHorizontal()
+        public static float GetAxisHorizontal()
         {
             if (m_Axes[HORIZONTAL_ID] != 0.0f)
                 return m_Axes[HORIZONTAL_ID];
             return Input.GetAxis(HORIZONTAL);
         }
 
-        public void SetAxisHorizontal(float value)
+        public static void SetAxisHorizontal(float value)
         {
             m_Axes[HORIZONTAL_ID] = value;
         }
 
-        public float GetAxisRawHorizontal()
+        public static float GetAxisRawHorizontal()
         {
             if (m_Axes[HORIZONTAL_ID] != 0.0f)
                 return m_Axes[HORIZONTAL_ID];
             return Input.GetAxisRaw(HORIZONTAL);
         }
 
-        public void SetAxisRawHorizontal(float value)
+        public static void SetAxisRawHorizontal(float value)
         {
             m_Axes[HORIZONTAL_ID] = value;
         }
 
-        public float GetAxisVertical()
+        public static float GetAxisVertical()
         {
             if (m_Axes[VERTICAL_ID] != 0.0f)
                 return m_Axes[VERTICAL_ID];
             return Input.GetAxis(VERTICAL);
         }
 
-        public void SetAxisVertical(float value)
+        public static void SetAxisVertical(float value)
         {
             m_Axes[VERTICAL_ID] = value;
         }
 
-        public float GetAxisRawVertical()
+        public static float GetAxisRawVertical()
         {
             if (m_Axes[VERTICAL_ID] != 0.0f)
                 return m_Axes[VERTICAL_ID];
             return Input.GetAxisRaw(VERTICAL);
         }
 
-        public void SetAxisRawVertical(float value)
+        public static void SetAxisRawVertical(float value)
         {
             m_Axes[VERTICAL_ID] = value;
         }
 
-        public bool GetKey(KeyCode key)
+        public static bool GetKey(KeyCode key)
         {
             if (m_Keys.TryGetValue(key, out VirtualButton virtualKey))
                 return virtualKey.IsPressed;
             return Input.GetKey(key);
         }
 
-        public bool GetKeyDown(KeyCode key)
+        public static bool GetKeyDown(KeyCode key)
         {
             if (m_Keys.TryGetValue(key, out VirtualButton virtualKey))
                 return virtualKey.IsDown;
             return Input.GetKeyDown(key);
         }
 
-        public void SetKeyDown(KeyCode key)
+        public static void SetKeyDown(KeyCode key)
         {
             GetEnsuredKey(key).Pressed();
         }
 
-        public bool GetKeyUp(KeyCode key)
+        public static bool GetKeyUp(KeyCode key)
         {
             if (m_Keys.TryGetValue(key, out VirtualButton virtualKey))
                 return virtualKey.IsUp;
             return Input.GetKeyUp(key);
         }
 
-        public void SetKeyUp(KeyCode key)
+        public static void SetKeyUp(KeyCode key)
         {
             GetEnsuredKey(key).Released();
         }
 
-        public void RemoveKey(KeyCode key)
+        public static void RemoveKey(KeyCode key)
         {
             m_Keys.Remove(key);
         }
 
-        public bool GetButton(string buttonName)
+        public static bool GetButton(string buttonName)
         {
             if (m_Buttons.TryGetValue(buttonName, out VirtualButton virtualKey))
                 return virtualKey.IsPressed;
             return Input.GetKey(buttonName);
         }
 
-        public bool GetButtonDown(string buttonName)
+        public static bool GetButtonDown(string buttonName)
         {
             if (m_Buttons.TryGetValue(buttonName, out VirtualButton virtualKey))
                 return virtualKey.IsDown;
             return Input.GetKeyDown(buttonName);
         }
 
-        public void SetButtonDown(string buttonName)
+        public static void SetButtonDown(string buttonName)
         {
             GetEnsuredButton(buttonName).Pressed();
         }
 
-        public bool GetButtonUp(string buttonName)
+        public static bool GetButtonUp(string buttonName)
         {
             if (m_Buttons.TryGetValue(buttonName, out VirtualButton virtualKey))
                 return virtualKey.IsUp;
             return Input.GetKeyUp(buttonName);
         }
 
-        public void SetButtonUp(string buttonName)
+        public static void SetButtonUp(string buttonName)
         {
             GetEnsuredButton(buttonName).Released();
         }
 
-        public void RemoveButton(string buttonName)
+        public static void RemoveButton(string buttonName)
         {
             m_Buttons.Remove(buttonName);
         }
 
-        public Vector3 GetMousePosition()
+        public static Vector3 GetMousePosition()
         {
             if (!Mathx.IsZero(m_MousePosition))
                 return m_MousePosition;
             return Input.mousePosition;
         }
 
-        public void SetMousePosition(Vector3 position)
+        public static void SetMousePosition(Vector3 position)
         {
             m_MousePosition = position;
         }
 
-        private VirtualButton GetEnsuredKey(KeyCode key)
+        private static VirtualButton GetEnsuredKey(KeyCode key)
         {
             if (m_Keys.TryGetValue(key, out VirtualButton result))
                 return result;
@@ -157,7 +157,7 @@ namespace Common
             return result;
         }
 
-        private VirtualButton GetEnsuredButton(string buttonName)
+        private static VirtualButton GetEnsuredButton(string buttonName)
         {
             if (m_Buttons.TryGetValue(buttonName, out VirtualButton result))
                 return result;
---
+++ Property.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Common
+{
+    public struct Property<T>
+    {
+        private readonly Func<T> _getter;
+        private readonly Action<T> _setter;
+
+        public delegate void Alteration(ref T value);
+
+        public Property(Func<T> getter, Action<T> setter)
+        {
+            _getter = getter;
+            _setter = setter;
+        }
+
+        public T Value
+        {
+            get => _getter();
+            set => _setter(value);
+        }
+
+        public void Alter(Alteration method)
+        {
+            var value = _getter();
+            method(ref value);
+            _setter(value);
+        }
+    }
+}
