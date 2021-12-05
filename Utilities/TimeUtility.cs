3721956af1efc38938217c6e2cfcbeec10617391
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-12-05 11:36:25 +0100

    Added TimeUtility

---
+++ Utilities/TimeUtility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public static class TimeUtility
+    {
+        public static float GetTime(bool unscaled)
+        {
+            return unscaled ? Time.unscaledTime : Time.time;
+        }
+
+        public static float GetDeltaTime(bool unscaled)
+        {
+            return unscaled ? Time.unscaledDeltaTime : Time.deltaTime;
+        }
+
+        public static float GetFixedTime(bool unscaled)
+        {
+            return unscaled ? Time.fixedUnscaledTime : Time.fixedTime;
+        }
+
+        public static float GetFixedDeltaTime(bool unscaled)
+        {
+            return unscaled ? Time.fixedUnscaledDeltaTime : Time.fixedDeltaTime;
+        }
+    }
+}
