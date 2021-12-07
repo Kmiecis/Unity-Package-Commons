bde4a038fd8638d8b21eeb7336dfc5499de2f0e4
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-12-07 06:29:37 +0100

    NamedArray attribute

---
+++ Attributes/Editor/NamedArrayDrawer.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using Common;
+using UnityEditor;
+using UnityEngine;
+
+namespace CommonEditor
+{
+    [CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+    public class NamedArrayDrawer : PropertyDrawer
+    {
+        private NamedArrayAttribute Attribute
+        {
+            get => (NamedArrayAttribute)attribute;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (
+                int.TryParse(property.propertyPath.Split('[', ']')[1], out int index) &&
+                index < Attribute.names.Length
+            )
+            {
+                label = new GUIContent(Attribute.names[index]);
+            }
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
+#endif
---
+++ Attributes/NamedArrayAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public class NamedArrayAttribute : PropertyAttribute
+    {
+        public readonly string[] names;
+
+        public NamedArrayAttribute(params string[] names)
+        {
+            this.names = names;
+        }
+    }
+}
