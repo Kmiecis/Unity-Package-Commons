f9e80c97b658df1434e18d31db062c07cead33a9
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-12-17 05:58:25 +0100

    Added TryGetComponents support

--- Extensions/ComponentExtensions.cs
+++ Extensions/ComponentExtensions.cs
@@ -14,9 +14,37 @@ namespace Common.Extensions
             where T : Component
         {
             if (self.TryGetComponent<T>(out var component))
-            {
                 Object.Destroy(component);
-            }
+        }
+        
+        public static bool TryGetComponentInChildren<T>(this Component self, out T component)
+        {
+            component = self.GetComponentInChildren<T>();
+            return component != null;
+        }
+
+        public static bool TryGetComponentInParent<T>(this Component self, out T component)
+        {
+            component = self.GetComponentInParent<T>();
+            return component != null;
+        }
+
+        public static bool TryGetComponents<T>(this Component self, out T[] components)
+        {
+            components = self.GetComponents<T>();
+            return !components.IsNullOrEmpty();
+        }
+
+        public static bool TryGetComponentsInChildren<T>(this Component self, out T[] components)
+        {
+            components = self.GetComponentsInChildren<T>();
+            return !components.IsNullOrEmpty();
+        }
+
+        public static bool TryGetComponentsInParent<T>(this Component self, out T[] components)
+        {
+            components = self.GetComponentsInParent<T>();
+            return !components.IsNullOrEmpty();
         }
     }
 }
--- Extensions/GameObjectExtensions.cs
+++ Extensions/GameObjectExtensions.cs
@@ -9,12 +9,54 @@ namespace Common.Extensions
             return self.scene.rootCount == 0;
         }
 
+        public static bool HasComponent<T>(this GameObject self)
+        {
+            return self.GetComponent<T>() != null;
+        }
+
+        public static void RemoveComponent<T>(this GameObject self)
+            where T : Component
+        {
+            if (self.TryGetComponent<T>(out var component))
+                Object.Destroy(component);
+        }
+
         public static T EnsureComponent<T>(this GameObject self)
             where T : Component
         {
             if (!self.TryGetComponent<T>(out T component))
                 component = self.AddComponent<T>();
             return component;
+        }
+
+        public static bool TryGetComponentInChildren<T>(this GameObject self, out T component)
+        {
+            component = self.GetComponentInChildren<T>();
+            return component != null;
+        }
+
+        public static bool TryGetComponentInParent<T>(this GameObject self, out T component)
+        {
+            component = self.GetComponentInParent<T>();
+            return component != null;
+        }
+
+        public static bool TryGetComponents<T>(this GameObject self, out T[] components)
+        {
+            components = self.GetComponents<T>();
+            return !components.IsNullOrEmpty();
+        }
+
+        public static bool TryGetComponentsInChildren<T>(this GameObject self, out T[] components)
+        {
+            components = self.GetComponentsInChildren<T>();
+            return !components.IsNullOrEmpty();
+        }
+
+        public static bool TryGetComponentsInParent<T>(this GameObject self, out T[] components)
+        {
+            components = self.GetComponentsInParent<T>();
+            return !components.IsNullOrEmpty();
         }
     }
 }
