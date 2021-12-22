4ee375fa3e16cf2fc89c49a2d09e6e0b6ce60cd5
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2021-12-22 06:24:50 +0100

    Basic pool implementation

---
+++ Pooling/BehaviourPool.cs
@@ -0,0 +1,27 @@
+﻿using Common.Extensions;
+using UnityEngine;
+
+namespace Common.Pooling
+{
+    public class BehaviourPool<T> : APool<T>
+        where T : MonoBehaviour
+    {
+        protected readonly T _prefab;
+
+        public BehaviourPool(int capacity, T prefab) :
+            base(capacity)
+        {
+            _prefab = prefab;
+        }
+
+        public override T Construct()
+        {
+            return Object.Instantiate(_prefab);
+        }
+
+        public override void Destroy(T item)
+        {
+            item.gameObject.Destroy();
+        }
+    }
+}
