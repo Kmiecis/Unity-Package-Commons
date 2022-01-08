34b1da740debe74327086ae903234d854ee50373
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-01-08 23:47:57 +0100

    Added array wrapper for multi-dimensional purposes

---
+++ Collections/Array2.cs
@@ -0,0 +1,87 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Common.Collections
+{
+    /// <summary> Array wrapper for two-dimensional grids </summary>
+    public sealed class Array2<T>
+    {
+        private T[] _array;
+        private int _width;
+        private int _height;
+        
+        public Array2(T[] array, int width, int height)
+        {
+            _array = array;
+            _width = width;
+            _height = height;
+        }
+
+        public Array2(int width, int height) :
+            this(new T[width * height], width, height)
+        {
+        }
+
+        public T[] Raw
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _array;
+        }
+
+        public int Length
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _array.Length;
+        }
+
+        public int Width
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _width;
+        }
+
+        public int Height
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _height;
+        }
+
+        /// <summary> Resizes array and returns its previous buffer </summary>
+        public T[] Resize(int width, int height)
+        {
+            var array = _array;
+            _array = new T[width * height];
+            _width = width;
+            _height = height;
+            return array;
+        }
+
+        public T this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _array[index];
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => _array[index] = value;
+        }
+
+        public T this[int x, int y]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => this[ToIndex(x, y)];
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => this[ToIndex(x, y)] = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int ToIndex(int x, int y)
+        {
+            return y * _width + x;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromIndex(int i, out int x, out int y)
+        {
+            x = i % _width;
+            y = i / _width;
+        }
+    }
+}
---
+++ Collections/Array3.cs
@@ -0,0 +1,97 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Common.Collections
+{
+    /// <summary> Array wrapper for three-dimensional grids </summary>
+    public sealed class Array3<T>
+    {
+        private T[] _array;
+        private int _width;
+        private int _height;
+        private int _depth;
+
+        public Array3(T[] array, int width, int height, int depth)
+        {
+            _array = array;
+            _width = width;
+            _height = height;
+            _depth = depth;
+        }
+
+        public Array3(int width, int height, int depth) :
+            this(new T[width * height * depth], width, height, depth)
+        {
+        }
+
+        public T[] Raw
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _array;
+        }
+
+        public int Length
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _array.Length;
+        }
+
+        public int Width
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _width;
+        }
+
+        public int Height
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _height;
+        }
+
+        public int Depth
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _depth;
+        }
+
+        /// <summary> Resizes array and returns its previous buffer </summary>
+        public T[] Resize(int width, int height, int depth)
+        {
+            var array = _array;
+            _array = new T[width * height * depth];
+            _width = width;
+            _height = height;
+            _depth = depth;
+            return array;
+        }
+
+        public T this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _array[index];
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => _array[index] = value;
+        }
+
+        public T this[int x, int y, int z]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => this[ToIndex(x, y, z)];
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => this[ToIndex(x, y, z)] = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int ToIndex(int x, int y, int z)
+        {
+            return ((z * _height) + y) * _width + x;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void FromIndex(int i, out int x, out int y, out int z)
+        {
+            x = i % _width;
+            y = (i / _width) % _height;
+            z = i / (_width * _height);
+        }
+    }
+}
