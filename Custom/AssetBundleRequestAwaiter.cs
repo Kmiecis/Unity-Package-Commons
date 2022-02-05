2b15c6242a88a9a6cb9c4260001ebaeed20fa94b
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-02-05 19:01:31 +0100

    Unity request awaiters

---
+++ Custom/AssetBundleRequestAwaiter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Common
+{
+    public class AssetBundleRequestAwaiter : INotifyCompletion
+    {
+        private readonly AssetBundleRequest _request;
+
+        public AssetBundleRequestAwaiter(AssetBundleRequest request)
+        {
+            _request = request;
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            _request.completed += _ => continuation();
+        }
+
+        public bool IsCompleted
+        {
+            get => _request.isDone;
+        }
+
+        public AssetBundleRequest GetResult()
+        {
+            return _request;
+        }
+    }
+
+    public static class AssetBundleRequestAwaiterExtensions
+    {
+        public static AssetBundleRequestAwaiter GetAwaiter(this AssetBundleRequest self)
+        {
+            return new AssetBundleRequestAwaiter(self);
+        }
+    }
+}
---
+++ Custom/UnityWebRequestAwaiter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine.Networking;
+
+namespace Common
+{
+    public class UnityWebRequestAwaiter : INotifyCompletion
+    {
+        private readonly UnityWebRequestAsyncOperation _operation;
+
+        public UnityWebRequestAwaiter(UnityWebRequestAsyncOperation operation)
+        {
+            _operation = operation;
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            _operation.completed += _ => continuation();
+        }
+
+        public bool IsCompleted
+        {
+            get => _operation.isDone;
+        }
+
+        public UnityWebRequest GetResult()
+        {
+            return _operation.webRequest;
+        }
+    }
+
+    public static class UnityWebRequestAwaiterExtensions
+    {
+        public static UnityWebRequestAwaiter GetAwaiter(this UnityWebRequestAsyncOperation self)
+        {
+            return new UnityWebRequestAwaiter(self);
+        }
+    }
+}
