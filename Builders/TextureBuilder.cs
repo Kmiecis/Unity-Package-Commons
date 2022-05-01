40e100b35e1522c799d9009ea24a1cc8ab6b199c
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-05-01 14:19:29 +0200

    Split TextureBuilder to 2D and 3D

--- Builders/TextureBuilder.cs
+++ Builders/TextureBuilder.cs
@@ -5,10 +5,12 @@ namespace Common
 {
     public class TextureBuilder : IDisposable
     {
-        protected int _width = 1;
-        protected int _height = 1;
-        protected int _depth = 1;
         protected Color[] _pixels;
+
+        public TextureBuilder(Color[] pixels)
+        {
+            _pixels = pixels;
+        }
 
         public Color[] Pixels
         {
@@ -25,6 +27,30 @@ namespace Common
         {
             _pixels[i] = c;
         }
+        
+        public Color this[int i]
+        {
+            get => GetPixel(i);
+            set => SetPixel(i, value);
+        }
+        
+        public void Dispose()
+        {
+            _pixels = null;
+        }
+    }
+
+    public class Texture2DBuilder : TextureBuilder
+    {
+        private int _width;
+        private int _height;
+
+        public Texture2DBuilder(int width, int height) :
+            base(new Color[width * height])
+        {
+            _width = width;
+            _height = height;
+        }
 
         public Color GetPixel(int x, int y)
         {
@@ -34,6 +60,33 @@ namespace Common
         public void SetPixel(int x, int y, Color c)
         {
             _pixels[x + y * _width] = c;
+        }
+
+        public Color this[int x, int y]
+        {
+            get => GetPixel(x, y);
+            set => SetPixel(x, y, value);
+        }
+
+        public virtual void Overwrite(Texture2D texture)
+        {
+            texture.SetPixels(_pixels);
+            texture.Apply();
+        }
+    }
+
+    public class Texture3DBuilder : TextureBuilder
+    {
+        private int _width;
+        private int _height;
+        private int _depth;
+
+        public Texture3DBuilder(int width, int height, int depth) :
+            base(new Color[width * height * depth])
+        {
+            _width = width;
+            _height = height;
+            _depth = depth;
         }
 
         public Color GetPixel(int x, int y, int z)
@@ -46,54 +99,16 @@ namespace Common
             _pixels[x + y * _width + z * _height] = c;
         }
 
-        public Color this[int i]
-        {
-            get => GetPixel(i);
-            set => SetPixel(i, value);
-        }
-
-        public Color this[int x, int y]
-        {
-            get => GetPixel(x, y);
-            set => SetPixel(x, y, value);
-        }
-
         public Color this[int x, int y, int z]
         {
             get => GetPixel(x, y, z);
             set => SetPixel(x, y, z, value);
         }
 
-        public TextureBuilder(int width, int height)
-        {
-            _width = width;
-            _height = height;
-            _pixels = new Color[width * height];
-        }
-        
-        public TextureBuilder(int width, int height, int depth)
-        {
-            _width = width;
-            _height = height;
-            _depth = depth;
-            _pixels = new Color[width * height * depth];
-        }
-
-        public virtual void Overwrite(Texture2D texture)
-        {
-            texture.SetPixels(_pixels);
-            texture.Apply();
-        }
-
         public virtual void Overwrite(Texture3D texture)
         {
             texture.SetPixels(_pixels);
             texture.Apply();
-        }
-        
-        public void Dispose()
-        {
-            _pixels = null;
         }
     }
 }
