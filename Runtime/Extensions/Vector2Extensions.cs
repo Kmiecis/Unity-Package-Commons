a66d5c579a381e1fe164dd1005c5f3879b456b9c
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-08-23 06:00:13 +0200

    More Vector2 extensions

--- Runtime/Extensions/Vector2Extensions.cs
+++ Runtime/Extensions/Vector2Extensions.cs
@@ -6,9 +6,9 @@ namespace Common.Extensions
     public static class Vector2Extensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector3 _XY(this Vector2 v, float f = 0.0f)
+        public static Vector3 XY_(this Vector2 v, float f = 0.0f)
         {
-            return new Vector3(f, v.x, v.y);
+            return new Vector3(v.x, v.y, f);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -18,9 +18,27 @@ namespace Common.Extensions
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector3 XY_(this Vector2 v, float f = 0.0f)
+        public static Vector3 _XY(this Vector2 v, float f = 0.0f)
         {
-            return new Vector3(v.x, v.y, f);
+            return new Vector3(f, v.x, v.y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 YX_(this Vector2 v, float f = 0.0f)
+        {
+            return new Vector3(v.y, v.x, f);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 Y_X(this Vector2 v, float f = 0.0f)
+        {
+            return new Vector3(v.y, f, v.x);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 _YX(this Vector2 v, float f = 0.0f)
+        {
+            return new Vector3(f, v.y, v.x);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
--- Runtime/Extensions/Vector2IntExtensions.cs
+++ Runtime/Extensions/Vector2IntExtensions.cs
@@ -6,21 +6,45 @@ namespace Common.Extensions
     public static class Vector2IntExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector3Int X_Y(this Vector2Int v)
+        public static Vector3Int XY_(this Vector2Int v, int i = 0)
         {
-            return new Vector3Int(v.x, 0, v.y);
+            return new Vector3Int(v.x, v.y, i);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector3Int XY_(this Vector2Int v)
+        public static Vector3Int X_Y(this Vector2Int v, int i = 0)
         {
-            return new Vector3Int(v.x, v.y, 0);
+            return new Vector3Int(v.x, i, v.y);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector3Int _XY(this Vector2Int v)
+        public static Vector3Int _XY(this Vector2Int v, int i = 0)
         {
-            return new Vector3Int(0, v.x, v.y);
+            return new Vector3Int(i, v.x, v.y);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3Int YX_(this Vector2Int v, int i = 0)
+        {
+            return new Vector3Int(v.y, v.x, i);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3Int Y_X(this Vector2Int v, int i = 0)
+        {
+            return new Vector3Int(v.y, i, v.x);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3Int _YX(this Vector2Int v, int i = 0)
+        {
+            return new Vector3Int(i, v.y, v.x);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2Int YX(this Vector2Int v)
+        {
+            return new Vector2Int(v.y, v.x);
         }
     }
 }
