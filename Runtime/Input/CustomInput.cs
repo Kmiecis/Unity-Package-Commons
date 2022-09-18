9cd73c01a02b52d7a5af71586b675590177e6ee7
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-09-18 11:37:42 +0200

    Abstracted GetAxis

--- Runtime/Input/CustomInput.cs
+++ Runtime/Input/CustomInput.cs
@@ -7,68 +7,15 @@ namespace Common
 {
     public static class CustomInput
     {
-        private const string HORIZONTAL = "Horizontal";
-        private const string VERTICAL = "Vertical";
-        private const int HORIZONTAL_ID = 0;
-        private const int VERTICAL_ID = 1;
+        private static readonly Dictionary<KeyCode, VirtualButton> _keys = new Dictionary<KeyCode, VirtualButton>();
+        private static readonly Dictionary<string, VirtualButton> _buttons = new Dictionary<string, VirtualButton>();
+        private static readonly Dictionary<string, float> _axes = new Dictionary<string, float>();
 
-        private static readonly Dictionary<KeyCode, VirtualButton> m_Keys = new Dictionary<KeyCode, VirtualButton>();
-        private static readonly Dictionary<string, VirtualButton> m_Buttons = new Dictionary<string, VirtualButton>();
-
-        private static Vector2 m_Axes;
-        private static Vector3 m_MousePosition;
-
-        public static float GetAxisHorizontal()
-        {
-            if (m_Axes[HORIZONTAL_ID] != 0.0f)
-                return m_Axes[HORIZONTAL_ID];
-            return Input.GetAxis(HORIZONTAL);
-        }
-
-        public static void SetAxisHorizontal(float value)
-        {
-            m_Axes[HORIZONTAL_ID] = value;
-        }
-
-        public static float GetAxisRawHorizontal()
-        {
-            if (m_Axes[HORIZONTAL_ID] != 0.0f)
-                return m_Axes[HORIZONTAL_ID];
-            return Input.GetAxisRaw(HORIZONTAL);
-        }
-
-        public static void SetAxisRawHorizontal(float value)
-        {
-            m_Axes[HORIZONTAL_ID] = value;
-        }
-
-        public static float GetAxisVertical()
-        {
-            if (m_Axes[VERTICAL_ID] != 0.0f)
-                return m_Axes[VERTICAL_ID];
-            return Input.GetAxis(VERTICAL);
-        }
-
-        public static void SetAxisVertical(float value)
-        {
-            m_Axes[VERTICAL_ID] = value;
-        }
-
-        public static float GetAxisRawVertical()
-        {
-            if (m_Axes[VERTICAL_ID] != 0.0f)
-                return m_Axes[VERTICAL_ID];
-            return Input.GetAxisRaw(VERTICAL);
-        }
-
-        public static void SetAxisRawVertical(float value)
-        {
-            m_Axes[VERTICAL_ID] = value;
-        }
+        private static Vector3 _mousePosition = Vectors3.NaN;
 
         public static bool GetKey(KeyCode key)
         {
-            if (TryGetCheckedKey(key, out VirtualButton virtualKey))
+            if (TryGetCheckedKey(key, out var virtualKey))
                 return virtualKey.IsPressed;
             return Input.GetKey(key);
         }
@@ -80,26 +27,26 @@ namespace Common
 
         public static bool GetKeyDown(KeyCode key)
         {
-            if (TryGetCheckedKey(key, out VirtualButton virtualKey))
+            if (TryGetCheckedKey(key, out var virtualKey))
                 return virtualKey.IsDown;
             return Input.GetKeyDown(key);
         }
 
         public static bool GetKeyUp(KeyCode key)
         {
-            if (TryGetCheckedKey(key, out VirtualButton virtualKey))
+            if (TryGetCheckedKey(key, out var virtualKey))
                 return virtualKey.IsUp;
             return Input.GetKeyUp(key);
         }
 
-        public static void RemoveKey(KeyCode key)
+        public static bool RemoveKey(KeyCode key)
         {
-            m_Keys.Remove(key);
+            return _keys.Remove(key);
         }
 
         public static bool GetButton(string buttonName)
         {
-            if (TryGetCheckedButton(buttonName, out VirtualButton virtualKey))
+            if (TryGetCheckedButton(buttonName, out var virtualKey))
                 return virtualKey.IsPressed;
             return Input.GetKey(buttonName);
         }
@@ -111,33 +58,55 @@ namespace Common
 
         public static bool GetButtonDown(string buttonName)
         {
-            if (TryGetCheckedButton(buttonName, out VirtualButton virtualKey))
+            if (TryGetCheckedButton(buttonName, out var virtualKey))
                 return virtualKey.IsDown;
             return Input.GetKeyDown(buttonName);
         }
 
         public static bool GetButtonUp(string buttonName)
         {
-            if (TryGetCheckedButton(buttonName, out VirtualButton virtualKey))
+            if (TryGetCheckedButton(buttonName, out var virtualKey))
                 return virtualKey.IsUp;
             return Input.GetKeyUp(buttonName);
         }
 
-        public static void RemoveButton(string buttonName)
+        public static bool RemoveButton(string buttonName)
         {
-            m_Buttons.Remove(buttonName);
+            return _buttons.Remove(buttonName);
+        }
+
+        public static float GetAxis(string axisName)
+        {
+            if (_axes.TryGetValue(axisName, out var result))
+                return result;
+            return Input.GetAxis(axisName);
+        }
+
+        public static void SetAxis(string axisName, float value)
+        {
+            _axes[axisName] = value;
+        }
+
+        public static bool RemoveAxis(string axisName)
+        {
+            return _axes.Remove(axisName);
         }
 
         public static Vector3 GetMousePosition()
         {
-            if (!Mathx.IsZero(m_MousePosition))
-                return m_MousePosition;
+            if (!Mathx.IsNaN(_mousePosition))
+                return _mousePosition;
             return Input.mousePosition;
         }
 
         public static void SetMousePosition(Vector3 position)
         {
-            m_MousePosition = position;
+            _mousePosition = position;
+        }
+
+        public static void RemoveMousePosition()
+        {
+            _mousePosition = Vectors3.NaN;
         }
 
         private static VirtualButton GetEnsuredFrom<TKey>(Dictionary<TKey, VirtualButton> dictionary, TKey key)
@@ -147,36 +116,35 @@ namespace Common
 
         private static VirtualButton GetEnsuredKey(KeyCode key)
         {
-            return GetEnsuredFrom(m_Keys, key);
+            return GetEnsuredFrom(_keys, key);
         }
 
         private static VirtualButton GetEnsuredButton(string buttonName)
         {
-            return GetEnsuredFrom(m_Buttons, buttonName);
+            return GetEnsuredFrom(_buttons, buttonName);
         }
 
         private static bool TryGetCheckedFrom<TKey>(Dictionary<TKey, VirtualButton> dictionary, TKey key, out VirtualButton button)
         {
-            button = default;
             if (dictionary.TryGetValue(key, out button))
             {
                 if (!button.IsPressed && !button.IsUp)
                 {
                     dictionary.Remove(key);
-                    button = default;
+                    button = null;
                 }
             }
-            return button != default;
+            return button != null;
         }
 
         private static bool TryGetCheckedKey(KeyCode key, out VirtualButton button)
         {
-            return TryGetCheckedFrom(m_Keys, key, out button);
+            return TryGetCheckedFrom(_keys, key, out button);
         }
 
         private static bool TryGetCheckedButton(string buttonName, out VirtualButton button)
         {
-            return TryGetCheckedFrom(m_Buttons, buttonName, out button);
+            return TryGetCheckedFrom(_buttons, buttonName, out button);
         }
     }
 }
