9f58a713ab02ea8f0662a462dde090a4240cb15b
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-10-14 12:51:48 +0200

    Standard Physics2D Raycast

---
+++ Runtime/Utilities/UPhysics2D.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Common.Mathematics
+{
+    public static class UPhysics2D
+    {
+        public static bool Raycast(Vector2 origin, Vector2 direction, out RaycastHit2D hitInfo, float distance = float.PositiveInfinity, int layerMask = Physics2D.DefaultRaycastLayers)
+        {
+            hitInfo = Physics2D.Raycast(origin, direction, distance, layerMask);
+            return hitInfo;
+        }
+    }
+}
