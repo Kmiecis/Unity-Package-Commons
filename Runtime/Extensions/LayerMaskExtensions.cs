d4e6bf6cece45e0bb888f7a7812e57a8d4647267
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-10-20 05:50:01 +0200

    Created LayerMaskExtensions

---
+++ Runtime/Extensions/LayerMaskExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public static class LayerMaskExtensions
+    {
+        public static bool Contains(this LayerMask self, int layer)
+        {
+            return self == (self | (1 << layer));
+        }
+    }
+}
