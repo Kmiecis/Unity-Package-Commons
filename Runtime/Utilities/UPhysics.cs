0034874885f279af2aa1147364f432c7a1625d87
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-10-21 05:13:17 +0200

    RaycastIgnoreCollision

---
+++ Runtime/Utilities/UPhysics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Common
+{
+    public static class UPhysics
+    {
+        public static int RaycastDistanceComparer(RaycastHit left, RaycastHit right)
+        {
+            return Math.Sign(left.distance - right.distance);
+        }
+
+        public static bool RaycastIgnoreCollision(Vector3 origin, Vector3 direction, out RaycastHit hitInfo, Collider collider, float distance = float.PositiveInfinity, int layerMask = Physics.DefaultRaycastLayers)
+        {
+            var hits = Physics.RaycastAll(origin, direction, distance, layerMask);
+
+            if (hits.Length == 0)
+            {
+                hitInfo = default;
+                return false;
+            }
+
+            Array.Sort(hits, RaycastDistanceComparer);
+
+            var hit = hits[0];
+            if (ReferenceEquals(hit.collider, collider))
+            {
+                if (hits.Length > 1)
+                {
+                    hitInfo = hits[1];
+                    return true;
+                }
+
+                hitInfo = default;
+                return false;
+            }
+
+            hitInfo = hit;
+            return true;
+        }
+    }
+}
--- Runtime/Utilities/UPhysics2D.cs
+++ Runtime/Utilities/UPhysics2D.cs
@@ -1,13 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-namespace Common.Mathematics
+namespace Common
 {
     public static class UPhysics2D
     {
+        public static int RaycastDistanceComparer(RaycastHit2D left, RaycastHit2D right)
+        {
+            return Math.Sign(left.distance - right.distance);
+        }
+
         public static bool Raycast(Vector2 origin, Vector2 direction, out RaycastHit2D hitInfo, float distance = float.PositiveInfinity, int layerMask = Physics2D.DefaultRaycastLayers)
         {
             hitInfo = Physics2D.Raycast(origin, direction, distance, layerMask);
             return hitInfo;
+        }
+
+        public static bool RaycastIgnoreCollision(Vector2 origin, Vector2 direction, out RaycastHit2D hitInfo, Collider2D collider, float distance = float.PositiveInfinity, int layerMask = Physics2D.DefaultRaycastLayers)
+        {
+            var hits = Physics2D.RaycastAll(origin, direction, distance, layerMask);
+
+            if (hits.Length == 0)
+            {
+                hitInfo = default;
+                return false;
+            }
+
+            Array.Sort(hits, RaycastDistanceComparer);
+
+            var hit = hits[0];
+            if (ReferenceEquals(hit.collider, collider))
+            {
+                if (hits.Length > 1)
+                {
+                    hitInfo = hits[1];
+                    return true;
+                }
+
+                hitInfo = default;
+                return false;
+            }
+
+            hitInfo = hit;
+            return true;
         }
     }
 }
