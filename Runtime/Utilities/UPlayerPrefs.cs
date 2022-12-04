859e33ce37a090a4862ce5e9c1588be433ce07c9
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-12-04 10:42:49 +0100

    Player prefs utility

---
+++ Runtime/Utilities/UPlayerPrefs.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Common
+{
+    public static class UPlayerPrefs
+    {
+        public static bool TryGetInt(string key, out int value)
+        {
+            value = PlayerPrefs.GetInt(key);
+            return PlayerPrefs.HasKey(key);
+        }
+
+        public static bool TryGetFloat(string key, out float value)
+        {
+            value = PlayerPrefs.GetFloat(key);
+            return PlayerPrefs.HasKey(key);
+        }
+
+        public static bool TryGetString(string key, out string value)
+        {
+            value = PlayerPrefs.GetString(key);
+            return PlayerPrefs.HasKey(key);
+        }
+    }
+}
