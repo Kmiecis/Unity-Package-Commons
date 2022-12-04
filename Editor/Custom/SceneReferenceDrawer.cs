baffe47af4064cc1225041576a36a9cc50cce34d
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-12-04 14:50:56 +0100

    SceneReference

---
+++ Editor/Custom/SceneReferenceDrawer.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using UnityEditor;
+using UnityEngine;
+
+namespace CommonEditor
+{
+    [CustomPropertyDrawer(typeof(SceneReference))]
+    public class SceneReferenceDrawer : PropertyDrawer
+    {
+        private const string SCENE_PROPERTY_NAME = "_scene";
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            position = EditorGUI.PrefixLabel(position, label);
+
+            var sceneProperty = property.FindPropertyRelative(SCENE_PROPERTY_NAME);
+            EditorGUI.PropertyField(position, sceneProperty, GUIContent.none);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, false);
+        }
+    }
+}
