49327d936e1530e3184b3118816f69e23b84b2c3
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-12-13 05:04:05 +0100

    Path Utilities

---
+++ Runtime/Utilities/UPath.cs
@@ -0,0 +1,43 @@
+﻿using Common.Extensions;
+
+namespace Common
+{
+    public static class UPath
+    {
+        public static string GetPathTo(string path, string directory)
+        {
+            if (path.TryIndexOf(directory, out int i))
+            {
+                return path.Substring(0, i - 1);
+            }
+            return null;
+        }
+
+        public static string GetPathFrom(string path, string directory)
+        {
+            if (path.TryIndexOf(directory, out int index))
+            {
+                return path.Substring(index + directory.Length + 1);
+            }
+            return null;
+        }
+
+        public static string GetSubPath(string path, string directory)
+        {
+            if (path.TryIndexOf(directory, out int i))
+            {
+                return path.Substring(i);
+            }
+            return null;
+        }
+
+        public static string GetPathWithoutExtension(string path)
+        {
+            if (path.TryLastIndexOf('.', out int i))
+            {
+                return path.Substring(0, i);
+            }
+            return path;
+        }
+    }
+}
