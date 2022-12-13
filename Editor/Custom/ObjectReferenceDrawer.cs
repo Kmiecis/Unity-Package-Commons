af0abe7849c068ae030d2ae90a430714155a11c3
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2022-12-13 05:07:51 +0100

    Object reference script

---
+++ Editor/Custom/ObjectReferenceDrawer.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using UnityEditor;
+using UnityEngine;
+
+namespace CommonEditor
+{
+    [CustomPropertyDrawer(typeof(ObjectReference))]
+    public class ObjectReferenceDrawer : PropertyDrawer
+    {
+        private const string OBJECT_PROPERTY_NAME = "_value";
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            position = EditorGUI.PrefixLabel(position, label);
+
+            var sceneProperty = property.FindPropertyRelative(OBJECT_PROPERTY_NAME);
+            EditorGUI.PropertyField(position, sceneProperty, GUIContent.none);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, false);
+        }
+    }
+}
---
+++ Runtime/Custom/ObjectReference.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Common
+{
+    [Serializable]
+    public class ObjectReference : ISerializationCallbackReceiver
+    {
+        private const string RESOURCES = "Resources";
+
+#if UNITY_EDITOR
+        [SerializeField]
+        private Object _value;
+#endif
+        [SerializeField, HideInInspector]
+        private string _path;
+
+        public string AssetPath
+        {
+            get
+            {
+#if UNITY_EDITOR
+                UpdatePath();
+#endif
+                return _path;
+            }
+        }
+
+        public string AssetName
+        {
+            get
+            {
+                return Path.GetFileName(AssetPath);
+            }
+        }
+
+        public string ResourcePath
+        {
+            get
+            {
+                return UPath.GetPathFrom(AssetPath, RESOURCES);
+            }
+        }
+
+#if UNITY_EDITOR
+        private void UpdatePath()
+        {
+            if (_value == null)
+            {
+                _path = null;
+            }
+            else
+            {
+                var assetPath = AssetDatabase.GetAssetPath(_value);
+                _path = UPath.GetPathWithoutExtension(assetPath);
+            }
+        }
+#endif
+
+        public static implicit operator string(ObjectReference value)
+        {
+            return value.AssetName;
+        }
+
+        #region ISerializationCallbackReceiver
+        public void OnBeforeSerialize()
+        {
+#if UNITY_EDITOR
+            UpdatePath();
+#endif
+        }
+
+        public void OnAfterDeserialize()
+        {
+#if UNITY_EDITOR
+            void UpdateNameOnNextUpdate()
+            {
+                EditorApplication.update -= UpdateNameOnNextUpdate;
+                UpdatePath();
+            }
+
+            EditorApplication.update += UpdateNameOnNextUpdate;
+#endif
+        }
+        #endregion
+    }
+}
