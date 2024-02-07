d9354a75cfc209eb03c3711766b7e9001373dc63
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-02-07 05:26:48 +0100

    Fixed NextLong

--- Runtime/Extensions/RandomExtensions.cs
+++ Runtime/Extensions/RandomExtensions.cs
@@ -64,16 +64,9 @@ namespace Common.Extensions
 
         public static long NextLong(this Random self, long min, long max)
         {
-            ulong range = (ulong)(max - min);
-            var bytes = new byte[8];
-            ulong rand;
-            do
-            {
-                self.NextBytes(bytes);
-                rand = (ulong)BitConverter.ToInt64(bytes, 0);
-            }
-            while (rand > ulong.MaxValue - ((ulong.MaxValue % range) + 1) % range);
-            return (long)(rand % range) + min;
+            var value = self.NextDouble();
+            var range = (ulong)(max - min);
+            return (long)(value * range + min);
         }
 
         public static long NextLong(this Random self, long max)
