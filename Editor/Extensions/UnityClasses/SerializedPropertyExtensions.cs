7f42ff04d6d8808f37933b054b8a29070d4285ae
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-02-13 04:08:00 +0100

    Setting value

--- Editor/Extensions/UnityClasses/SerializedPropertyExtensions.cs
+++ Editor/Extensions/UnityClasses/SerializedPropertyExtensions.cs
@@ -123,145 +123,6 @@ namespace CommonEditor.Extensions
             return typeof(object);
         }
 
-        public static object GetTypeValue(this SerializedProperty self)
-        {
-            switch (self.propertyType)
-            {
-                case SerializedPropertyType.AnimationCurve:
-                    return self.animationCurveValue;
-                case SerializedPropertyType.ArraySize:
-                    return self.arraySize;
-                case SerializedPropertyType.Boolean:
-                    return self.boolValue;
-                case SerializedPropertyType.Bounds:
-                    return self.boundsValue;
-                case SerializedPropertyType.BoundsInt:
-                    return self.boundsIntValue;
-                case SerializedPropertyType.Character:
-                    return self.intValue;
-                case SerializedPropertyType.Color:
-                    return self.colorValue;
-                case SerializedPropertyType.Enum:
-                    goto case SerializedPropertyType.Integer;
-                case SerializedPropertyType.ExposedReference:
-                    return self.exposedReferenceValue;
-                case SerializedPropertyType.FixedBufferSize:
-                    return self.intValue;
-                case SerializedPropertyType.Float:
-                    return self.floatValue;
-                case SerializedPropertyType.Hash128:
-                    return self.hash128Value;
-                case SerializedPropertyType.Integer:
-                    return self.intValue;
-                case SerializedPropertyType.LayerMask:
-                    return self.intValue;
-                case SerializedPropertyType.ManagedReference:
-                    return self.managedReferenceValue;
-                case SerializedPropertyType.ObjectReference:
-                    return self.objectReferenceValue;
-                case SerializedPropertyType.Quaternion:
-                    return self.quaternionValue;
-                case SerializedPropertyType.Rect:
-                    return self.rectValue;
-                case SerializedPropertyType.RectInt:
-                    return self.rectIntValue;
-                case SerializedPropertyType.String:
-                    return self.stringValue;
-                case SerializedPropertyType.Vector2:
-                    return self.vector2Value;
-                case SerializedPropertyType.Vector2Int:
-                    return self.vector2IntValue;
-                case SerializedPropertyType.Vector3:
-                    return self.vector3Value;
-                case SerializedPropertyType.Vector3Int:
-                    return self.vector3IntValue;
-                case SerializedPropertyType.Vector4:
-                    return self.vector4Value;
-            }
-
-            throw new System.NotSupportedException(string.Format("The GetValue method failed on \"{0}\" because it has an unsupported propertyType {1}.", self.propertyPath, self.propertyType));
-        }
-
-        public static void SetTypeValue(this SerializedProperty self, object value)
-        {
-            switch (self.propertyType)
-            {
-                case SerializedPropertyType.AnimationCurve:
-                    self.animationCurveValue = (AnimationCurve)value;
-                    break;
-                case SerializedPropertyType.ArraySize:
-                    self.arraySize = (int)value;
-                    break;
-                case SerializedPropertyType.Boolean:
-                    self.boolValue = (bool)value;
-                    break;
-                case SerializedPropertyType.Bounds:
-                    self.boundsValue = (Bounds)value;
-                    break;
-                case SerializedPropertyType.BoundsInt:
-                    self.boundsIntValue = (BoundsInt)value;
-                    break;
-                case SerializedPropertyType.Character:
-                    goto case SerializedPropertyType.Integer;
-                case SerializedPropertyType.Color:
-                    self.colorValue = (Color)value;
-                    break;
-                case SerializedPropertyType.Enum:
-                    goto case SerializedPropertyType.Integer;
-                case SerializedPropertyType.ExposedReference:
-                    self.exposedReferenceValue = (Object)value;
-                    break;
-                case SerializedPropertyType.FixedBufferSize:
-                    goto case SerializedPropertyType.Integer;
-                case SerializedPropertyType.Float:
-                    self.floatValue = (float)value;
-                    break;
-                case SerializedPropertyType.Hash128:
-                    self.hash128Value = (Hash128)value;
-                    break;
-                case SerializedPropertyType.Integer:
-                    self.intValue = (int)value;
-                    break;
-                case SerializedPropertyType.LayerMask:
-                    goto case SerializedPropertyType.Integer;
-                case SerializedPropertyType.ManagedReference:
-                    self.managedReferenceValue = value;
-                    break;
-                case SerializedPropertyType.ObjectReference:
-                    self.objectReferenceValue = (Object)value;
-                    break;
-                case SerializedPropertyType.Quaternion:
-                    self.quaternionValue = (Quaternion)value;
-                    break;
-                case SerializedPropertyType.Rect:
-                    self.rectValue = (Rect)value;
-                    break;
-                case SerializedPropertyType.RectInt:
-                    self.rectIntValue = (RectInt)value;
-                    break;
-                case SerializedPropertyType.String:
-                    self.stringValue = (string)value;
-                    break;
-                case SerializedPropertyType.Vector2:
-                    self.vector2Value = (Vector2)value;
-                    break;
-                case SerializedPropertyType.Vector2Int:
-                    self.vector2IntValue = (Vector2Int)value;
-                    break;
-                case SerializedPropertyType.Vector3:
-                    self.vector3Value = (Vector3)value;
-                    break;
-                case SerializedPropertyType.Vector3Int:
-                    self.vector3IntValue = (Vector3Int)value;
-                    break;
-                case SerializedPropertyType.Vector4:
-                    self.vector4Value = (Vector4)value;
-                    break;
-            }
-
-            throw new System.NotSupportedException(string.Format("The SetValue method failed on \"{0}\" because it has an unsupported propertyType {1}.", self.propertyPath, self.propertyType));
-        }
-
         public static object GetValue(this SerializedProperty self)
         {
             var sanitizedPath = self.propertyPath.Replace(".Array.data[", "[");
@@ -272,10 +133,7 @@ namespace CommonEditor.Extensions
             {
                 if (element.Contains("["))
                 {
-                    var elementName = element.Substring(0, element.IndexOf("["));
-                    var strIndex = element.Substring(element.IndexOf("[")).Replace("[", string.Empty).Replace("]", string.Empty);
-                    var index = int.Parse(strIndex);
-                    result = GetValueFromList(result, elementName, index);
+                    result = GetValueFromList(result, element);
                 }
                 else
                 {
@@ -285,8 +143,46 @@ namespace CommonEditor.Extensions
             return result;
         }
 
-        private static object GetValueFromList(object source, string name, int index)
+        public static void SetValue(this SerializedProperty self, object value)
         {
+            var sanitizedPath = self.propertyPath.Replace(".Array.data[", "[");
+            object target = self.serializedObject.targetObject;
+
+            var element = (string)null;
+            var elements = sanitizedPath.Split('.');
+            for (int i = 0; i < elements.Length - 1; ++i)
+            {
+                element = elements[i];
+                if (element.Contains("["))
+                {
+                    target = GetValueFromList(target, element);
+                }
+                else
+                {
+                    target = GetValueFromType(target, element);
+                }
+            }
+
+            element = elements[element.Length - 1];
+            if (element.Contains("["))
+            {
+                SetValueToList(target, element, value);
+            }
+            else
+            {
+                SetValueToType(target, element, value);
+            }
+        }
+
+        private static object GetValueFromList(object source, string subpath)
+        {
+            var indexBegin = subpath.IndexOf("[");
+            var indexEnd = subpath.IndexOf("]");
+            var strIndex = subpath.Substring(indexBegin + 1, indexEnd - (indexBegin + 1));
+            var index = int.Parse(strIndex);
+
+            var name = subpath.Substring(0, indexBegin);
+
             var list = GetValueFromType(source, name) as System.Collections.IList;
             if (list != null)
             {
@@ -295,18 +191,36 @@ namespace CommonEditor.Extensions
             return null;
         }
 
+        private static void SetValueToList(object target, string subpath, object value)
+        {
+            var indexBegin = subpath.IndexOf("[");
+            var indexEnd = subpath.IndexOf("]");
+            var strIndex = subpath.Substring(indexBegin + 1, indexEnd - (indexBegin + 1));
+            var index = int.Parse(strIndex);
+
+            var name = subpath.Substring(0, indexBegin);
+
+            var list = GetValueFromType(target, name) as System.Collections.IList;
+            if (list != null)
+            {
+                list[index] = value;
+            }
+        }
+
         private static object GetValueFromType(object source, string name)
         {
             var type = source.GetType();
             while (type != null)
             {
-                var field = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                var fieldFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
+                var field = type.GetField(name, fieldFlags);
                 if (field != null)
                 {
                     return field.GetValue(source);
                 }
 
-                var property = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                var propertyFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase;
+                var property = type.GetProperty(name, propertyFlags);
                 if (property != null)
                 {
                     return property.GetValue(source, null);
@@ -315,6 +229,31 @@ namespace CommonEditor.Extensions
                 type = type.BaseType;
             }
             return null;
+        }
+
+        private static void SetValueToType(object target, string name, object value)
+        {
+            var type = target.GetType();
+            while (type != null)
+            {
+                var fieldFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
+                var field = type.GetField(name, fieldFlags);
+                if (field != null)
+                {
+                    field.SetValue(target, value);
+                    return;
+                }
+
+                var propertyFlags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase;
+                var property = type.GetProperty(name, propertyFlags);
+                if (property != null)
+                {
+                    property.SetValue(target, value);
+                    return;
+                }
+
+                type = type.BaseType;
+            }
         }
     }
 }
