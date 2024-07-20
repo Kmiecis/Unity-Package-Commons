8fa7162a8c1d22617c80c670fee39ced7c373538
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-07-20 21:21:14 +0200

    Added GetBool

--- Runtime/Utilities/UnityClasses/UPlayerPrefs.cs
+++ Runtime/Utilities/UnityClasses/UPlayerPrefs.cs
@@ -1,9 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Common
 {
     public static class UPlayerPrefs
     {
+        public static bool GetBool(string key, bool defaultValue = false)
+        {
+            var intValue = Convert.ToInt32(defaultValue);
+            intValue = PlayerPrefs.GetInt(key, intValue);
+            return Convert.ToBoolean(intValue);
+        }
+
+        public static bool TryGetBool(string key, out bool value)
+        {
+            value = GetBool(key);
+            return PlayerPrefs.HasKey(key);
+        }
+
         public static bool TryGetInt(string key, out int value)
         {
             value = PlayerPrefs.GetInt(key);
