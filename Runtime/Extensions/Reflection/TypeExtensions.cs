8d5d1d6cef3f8729c81b12c1c10535259de58ec8
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-09-06 18:12:46 +0200

    Read Delegates from Types

--- Runtime/Extensions/Reflection/TypeExtensions.cs
+++ Runtime/Extensions/Reflection/TypeExtensions.cs
@@ -115,5 +115,37 @@ namespace Common.Extensions
             }
             return null;
         }
+
+        public static T GetMethodDelegate<T>(this Type self, string name, BindingFlags bindingAttr, params Type[] parameters)
+            where T : Delegate
+        {
+            var method = self.GetMethod(name, bindingAttr, null, parameters, null);
+            return (T)method.CreateDelegate(typeof(T));
+        }
+
+        public static Action<T> GetMethodAction<T>(this Type self, string name, BindingFlags bindingAttr)
+        {
+            return self.GetMethodDelegate<Action<T>>(name, bindingAttr, typeof(T));
+        }
+
+        public static Action<T1, T2> GetMethodAction<T1, T2>(this Type self, string name, BindingFlags bindingAttr)
+        {
+            return self.GetMethodDelegate<Action<T1, T2>>(name, bindingAttr, typeof(T1), typeof(T2));
+        }
+
+        public static Func<T> GetMethodFunc<T>(this Type self, string name, BindingFlags bindingAttr)
+        {
+            return self.GetMethodDelegate<Func<T>>(name, bindingAttr);
+        }
+
+        public static Func<T, TResult> GetMethodFunc<T, TResult>(this Type self, string name, BindingFlags bindingAttr)
+        {
+            return self.GetMethodDelegate<Func<T, TResult>>(name, bindingAttr, typeof(T));
+        }
+
+        public static Func<T1, T2, TResult> GetMethodFunc<T1, T2, TResult>(this Type self, string name, BindingFlags bindingAttr)
+        {
+            return self.GetMethodDelegate<Func<T1, T2, TResult>>(name, bindingAttr, typeof(T1), typeof(T2));
+        }
     }
 }
