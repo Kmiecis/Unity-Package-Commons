852e0c6fd6ba6b560861ed50e40226cd00f0dcd1
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-10-05 05:10:18 +0200

    RendererExtensions

---
+++ Runtime/Extensions/UnityComponents/RendererExtensions.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common.Extensions
+{
+    public static class RendererExtensions
+    {
+        public static void SetSharedMaterials(this Renderer self, List<Material> materials)
+        {
+            self.sharedMaterials = materials.ToArray();
+        }
+
+        public static void AppendSharedMaterial(this Renderer self, Material material)
+        {
+            var materials = new List<Material>();
+            self.GetSharedMaterials(materials);
+            materials.Add(material);
+            self.SetSharedMaterials(materials);
+        }
+
+        public static void RemoveSharedMaterial(this Renderer self, Material material)
+        {
+            var materials = new List<Material>();
+            self.GetSharedMaterials(materials);
+            materials.Remove(material);
+            self.SetSharedMaterials(materials);
+        }
+    }
+}
