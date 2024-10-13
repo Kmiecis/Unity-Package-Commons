c63bbdcf18e63eac3a052e247852145306386a09
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-10-13 07:21:52 +0200

    Added Scoped LabelWidth

--- Editor/Utilities/UEditorGUIUtility.cs
+++ Editor/Utilities/UEditorGUIUtility.cs
@@ -1,8 +1,36 @@
+using System;
+using UnityEditor;
+
 namespace CommonEditor
 {
     public static class UEditorGUIUtility
     {
         public const float SpaceHeight = 2.0f;
         public const float IndentWidth = 10.0f;
+
+        public class LabelWidthScope : IDisposable
+        {
+            private readonly float _labelWidth;
+
+            public float LabelWidth
+            {
+                set => EditorGUIUtility.labelWidth = value;
+            }
+
+            public LabelWidthScope()
+            {
+                _labelWidth = EditorGUIUtility.labelWidth;
+            }
+
+            public LabelWidthScope(float labelWidth) : this()
+            {
+                LabelWidth = labelWidth;
+            }
+
+            public void Dispose()
+            {
+                EditorGUIUtility.labelWidth = _labelWidth;
+            }
+        }
     }
 }
