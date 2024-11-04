1bbeab692494a3a8f64acaf1fd0adbb7646d8027
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-11-04 22:04:56 +0100

    More GUI Scopes

--- Editor/Utilities/UGUI.cs
+++ Editor/Utilities/UGUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,6 +7,74 @@ namespace CommonEditor
     public static class UGUI
     {
         public const float SpaceHeight = 2.0f;
+
+        public class BackgroundColorScope : IDisposable
+        {
+            private readonly Color _color;
+
+            public BackgroundColorScope(Color color)
+            {
+                _color = GUI.backgroundColor;
+
+                GUI.backgroundColor = color;
+            }
+
+            public void Dispose()
+            {
+                GUI.backgroundColor = _color;
+            }
+        }
+
+        public class ContentColorScope : IDisposable
+        {
+            private readonly Color _color;
+
+            public ContentColorScope(Color color)
+            {
+                _color = GUI.contentColor;
+
+                GUI.contentColor = color;
+            }
+
+            public void Dispose()
+            {
+                GUI.contentColor = _color;
+            }
+        }
+
+        public class ColorScope : IDisposable
+        {
+            private readonly Color _color;
+
+            public ColorScope(Color color)
+            {
+                _color = GUI.color;
+
+                GUI.color = color;
+            }
+
+            public void Dispose()
+            {
+                GUI.color = _color;
+            }
+        }
+
+        public class EnabledScope : IDisposable
+        {
+            private readonly bool _enabled;
+
+            public EnabledScope(bool enabled)
+            {
+                _enabled = GUI.enabled;
+
+                GUI.enabled = enabled;
+            }
+
+            public void Dispose()
+            {
+                GUI.enabled = _enabled;
+            }
+        }
 
         public static bool Button(ref Rect position, string text)
         {
