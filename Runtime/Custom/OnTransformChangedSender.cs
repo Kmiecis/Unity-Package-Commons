e033dc57d9d90937beca803338ded2ef06b7355e
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-11-10 06:42:06 +0100

    Changed manual Watcher to automated Sender

---
+++ Runtime/Custom/OnTransformChangedSender.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+namespace Common
+{
+    [ExecuteAlways]
+    public class OnTransformChangedSender : MonoBehaviour
+    {
+        private const string MethodName = "OnTransformChanged";
+
+        private int _lastFrame;
+
+        private Vector3 _lastPosition;
+        private Vector3 _lastScale;
+        private Quaternion _lastRotation;
+
+        public void CheckChanged()
+        {
+            if (HasFrameChanged() && HasChanged())
+            {
+                SendChangedMessage();
+            }
+        }
+
+        public void OnChange()
+        {
+            _lastFrame = Time.frameCount;
+            _lastPosition = transform.localPosition;
+            _lastScale = transform.localScale;
+            _lastRotation = transform.localRotation;
+
+            SendChangedMessage();
+        }
+
+        private bool HasFrameChanged()
+        {
+            var previous = _lastFrame;
+            _lastFrame = Time.frameCount;
+            return _lastFrame != previous;
+        }
+
+        private bool HasChanged()
+        {
+            var currentPosition = transform.localPosition;
+            if (currentPosition != _lastPosition)
+            {
+                _lastPosition = currentPosition;
+                return true;
+            }
+
+            var currentScale = transform.localScale;
+            if (currentScale != _lastScale)
+            {
+                _lastScale = currentScale;
+                return true;
+            }
+
+            var currentRotation = transform.localRotation;
+            if (currentRotation != _lastRotation)
+            {
+                _lastRotation = currentRotation;
+                return true;
+            }
+
+            return false;
+        }
+
+        private void SendChangedMessage()
+        {
+            SendMessage(MethodName, SendMessageOptions.DontRequireReceiver);
+        }
+
+        #region Unity
+        private void Update()
+        {
+            CheckChanged();
+        }
+        #endregion
+    }
+}
