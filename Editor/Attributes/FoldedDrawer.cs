9579fc24a0feafc8be9ee419c45853731270cdec
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2024-11-15 05:53:46 +0100

    Folded attribute

---
+++ Editor/Attributes/FoldedDrawer.cs
@@ -0,0 +1,20 @@
+using Common;
+using UnityEditor;
+using UnityEngine;
+
+namespace CommonEditor
+{
+    [CustomPropertyDrawer(typeof(FoldedAttribute))]
+    public class FoldedDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
+    }
+}
