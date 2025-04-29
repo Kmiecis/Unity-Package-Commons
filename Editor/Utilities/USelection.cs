b31df77642a32a81c9ecd2e0102a2f7308928d24
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2025-04-29 08:49:24 +0200

    Update USelection.cs

--- Editor/Utilities/USelection.cs
+++ Editor/Utilities/USelection.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace CommonEditor
@@ -18,6 +19,51 @@ namespace CommonEditor
         public static string GetActivePath()
         {
             return AssetDatabase.GetAssetPath(Selection.activeObject);
+        }
+
+        public static bool TryGetActive(out Object active)
+        {
+            active = Selection.activeObject;
+            return active != null;
+        }
+
+        public static bool TryGetActive<T>(out T active)
+            where T : Object
+        {
+            active = Selection.activeObject as T;
+            return active != null;
+        }
+
+        public static IEnumerable<Object> GetSelected(bool warnings = false)
+        {
+            var selections = Selection.objects;
+            if (selections != null)
+            {
+                foreach (var selected in selections)
+                {
+                    yield return selected;
+                }
+            }
+            else if (warnings)
+            {
+                Debug.LogWarning("No objects selected");
+            }
+        }
+
+        public static IEnumerable<T> GetSelected<T>(bool warnings = false)
+            where T : Object
+        {
+            foreach (var selected in GetSelected(warnings))
+            {
+                if (selected is T casted)
+                {
+                    yield return casted;
+                }
+                else if (warnings)
+                {
+                    Debug.LogWarning($"Selected object {selected.name} is not of type {typeof(T)}");
+                }
+            }
         }
     }
 }
