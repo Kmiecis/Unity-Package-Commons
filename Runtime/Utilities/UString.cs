7d67a1d8954adf174e19fb57115ccce5cd1c6d1a
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2025-09-16 21:37:12 +0200

    Update UString.cs

--- Runtime/Utilities/UString.cs
+++ Runtime/Utilities/UString.cs
@@ -4,5 +4,6 @@ namespace Common
     {
         public const string NumberFormat = "+0;-0;0";
         public const string FloatFormat = "+0.0;-0.0;0.0";
+        public const string PercentageFormat = "+0%;-0%;0%";
     }
 }
