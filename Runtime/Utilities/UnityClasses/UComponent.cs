5efddf57a51d03598169da75058f64bccd378ff5
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2025-11-09 21:50:31 +0100

    Update UComponent.cs

--- Runtime/Utilities/UnityClasses/UComponent.cs
+++ Runtime/Utilities/UnityClasses/UComponent.cs
@@ -10,10 +10,30 @@ namespace Common
             UObject.Destroy(component);
         }
 
+        public static void Remove<T>(ref T component)
+            where T : Component
+        {
+            if (component != null)
+            {
+                Remove(component);
+                component = null;
+            }
+        }
+
         public static void Destroy<T>(T component)
             where T : Component
         {
             UObject.Destroy(component.gameObject);
+        }
+
+        public static void Destroy<T>(ref T component)
+            where T : Component
+        {
+            if (component != null)
+            {
+                Destroy(component);
+                component = null;
+            }
         }
     }
 }
