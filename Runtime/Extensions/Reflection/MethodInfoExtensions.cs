5790342a2c1f37659e3ed9543e4b4a7fcce5ce7c
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2025-11-23 13:11:27 +0100

    AsDelegate for non-static Methods

--- Runtime/Extensions/Reflection/MethodInfoExtensions.cs
+++ Runtime/Extensions/Reflection/MethodInfoExtensions.cs
@@ -5,9 +5,9 @@ namespace Common
 {
     public static class MethodInfoExtensions
     {
-        public static void Invoke(this MethodInfo self, object obj, params object[] parameters)
+        public static object Invoke(this MethodInfo self, object obj, params object[] parameters)
         {
-            self.Invoke(obj, parameters);
+            return self.Invoke(obj, parameters);
         }
 
         public static bool HasMatchingParameters(this MethodInfo self, params Type[] types)
@@ -36,9 +36,20 @@ namespace Common
             return (T)self.CreateDelegate(typeof(T));
         }
 
+        public static T AsDelegate<T>(this MethodInfo self, object target)
+            where T : Delegate
+        {
+            return (T)self.CreateDelegate(typeof(T), target);
+        }
+
         public static Action AsAction(this MethodInfo self)
         {
             return self.AsDelegate<Action>();
+        }
+
+        public static Action AsAction(this MethodInfo self, object target)
+        {
+            return self.AsDelegate<Action>(target);
         }
 
         public static Action<T> AsAction<T>(this MethodInfo self)
@@ -46,9 +57,19 @@ namespace Common
             return self.AsDelegate<Action<T>>();
         }
 
+        public static Action<T> AsAction<T>(this MethodInfo self, object target)
+        {
+            return self.AsDelegate<Action<T>>(target);
+        }
+
         public static Action<T1, T2> AsAction<T1, T2>(this MethodInfo self)
         {
             return self.AsDelegate<Action<T1, T2>>();
+        }
+
+        public static Action<T1, T2> AsAction<T1, T2>(this MethodInfo self, object target)
+        {
+            return self.AsDelegate<Action<T1, T2>>(target);
         }
 
         public static Func<TResult> AsFunc<TResult>(this MethodInfo self)
@@ -56,14 +77,29 @@ namespace Common
             return self.AsDelegate<Func<TResult>>();
         }
 
+        public static Func<TResult> AsFunc<TResult>(this MethodInfo self, object target)
+        {
+            return self.AsDelegate<Func<TResult>>(target);
+        }
+
         public static Func<T, TResult> AsFunc<T, TResult>(this MethodInfo self)
         {
             return self.AsDelegate<Func<T, TResult>>();
         }
 
+        public static Func<T, TResult> AsFunc<T, TResult>(this MethodInfo self, object target)
+        {
+            return self.AsDelegate<Func<T, TResult>>(target);
+        }
+
         public static Func<T1, T2, TResult> AsFunc<T1, T2, TResult>(this MethodInfo self)
         {
             return self.AsDelegate<Func<T1, T2, TResult>>();
+        }
+
+        public static Func<T1, T2, TResult> AsFunc<T1, T2, TResult>(this MethodInfo self, object target)
+        {
+            return self.AsDelegate<Func<T1, T2, TResult>>(target);
         }
     }
 }
