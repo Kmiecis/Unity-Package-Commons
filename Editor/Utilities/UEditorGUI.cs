6c55a2f1da26c4543f76c735df46d6d5cdef27a6
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2025-12-17 05:48:40 +0100

    Extracted GetHeight methods

--- Editor/Utilities/UEditorGUI.cs
+++ Editor/Utilities/UEditorGUI.cs
@@ -5,115 +5,221 @@ namespace CommonEditor
 {
     public static class UEditorGUI
     {
+        public static float GetBoundsHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Bounds, label ?? GUIContent.none);
+        }
+
+        public static float GetBoundsIntHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.BoundsInt, label ?? GUIContent.none);
+        }
+
+        public static float GetColorHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Color, label ?? GUIContent.none);
+        }
+
+        public static float GetCurveHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.AnimationCurve, label ?? GUIContent.none);
+        }
+
+        public static float GetDoubleHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Float, label ?? GUIContent.none);
+        }
+
+        public static float GetFloatHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Float, label ?? GUIContent.none);
+        }
+
+        public static float GetGradientHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Gradient, label ?? GUIContent.none);
+        }
+
+        public static float GetHeaderHeight()
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public static float GetIntHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Integer, label ?? GUIContent.none);
+        }
+
+        public static float GetLabelHeight()
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+
+        public static float GetLayerHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.LayerMask, label ?? GUIContent.none);
+        }
+
+        public static float GetMaskHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.LayerMask, label ?? GUIContent.none);
+        }
+
+        public static float GetObjectHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.ObjectReference, label ?? GUIContent.none);
+        }
+
+        public static float GetRectHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Rect, label ?? GUIContent.none);
+        }
+
+        public static float GetRectIntHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.RectInt, label ?? GUIContent.none);
+        }
+
+        public static float GetTextAreaHeight(GUIContent content, float width, GUIStyle style = null)
+        {
+            return (style ?? EditorStyles.textArea).CalcHeight(content, width);
+        }
+
+        public static float GetTextHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.String, label ?? GUIContent.none);
+        }
+
+        public static float GetVector2Height(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector2, label ?? GUIContent.none);
+        }
+
+        public static float GetVector2IntHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector2Int, label ?? GUIContent.none);
+        }
+
+        public static float GetVector3Height(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector3, label ?? GUIContent.none);
+        }
+
+        public static float GetVector3IntHeight(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector3Int, label ?? GUIContent.none);
+        }
+
+        public static float GetVector4Height(GUIContent label = null)
+        {
+            return EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector4, label ?? GUIContent.none);
+        }
+
         public static Bounds BoundsField(ref Rect position, Bounds value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Bounds, label);
-            var result = EditorGUI.BoundsField(position, label, value);
+            position.height = GetBoundsHeight(label);
+            var result = EditorGUI.BoundsField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static BoundsInt BoundsIntField(ref Rect position, BoundsInt value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.BoundsInt, label);
-            var result = EditorGUI.BoundsIntField(position, label, value);
+            position.height = GetBoundsIntHeight(label);
+            var result = EditorGUI.BoundsIntField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static Color ColorField(ref Rect position, Color value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Color, label);
-            var result = EditorGUI.ColorField(position, label, value);
+            position.height = GetColorHeight(label);
+            var result = EditorGUI.ColorField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static AnimationCurve CurveField(ref Rect position, AnimationCurve value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.AnimationCurve, label);
-            var result = EditorGUI.CurveField(position, label, value);
+            position.height = GetCurveHeight(label);
+            var result = EditorGUI.CurveField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static double DoubleField(ref Rect position, double value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Float, label);
-            var result = EditorGUI.DoubleField(position, label, value);
+            position.height = GetDoubleHeight(label);
+            var result = EditorGUI.DoubleField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
+        public static void DynamicHeaderField(ref Rect position, GUIContent label)
+        {
+            var style = new GUIStyle(EditorStyles.label) { wordWrap = true };
+            position.height = GetTextAreaHeight(label, position.width, style);
+            EditorGUI.LabelField(position, label);
+            position.y += position.height + UEditorGUIUtility.SpaceHeight;
+        }
+
         public static float FloatField(ref Rect position, float value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Float, label);
-            var result = EditorGUI.FloatField(position, label, value);
+            position.height = GetFloatHeight(label);
+            var result = EditorGUI.FloatField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static Gradient GradientField(ref Rect position, Gradient value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Gradient, label);
-            var result = EditorGUI.GradientField(position, label, value);
+            position.height = GetGradientHeight(label);
+            var result = EditorGUI.GradientField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static void HeaderField(ref Rect position, GUIContent label)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUIUtility.singleLineHeight;
+            position.height = GetHeaderHeight();
             EditorGUI.LabelField(position, label);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
         }
 
         public static int IntField(ref Rect position, int value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Integer, label);
-            var result = EditorGUI.IntField(position, label, value);
+            position.height = GetIntHeight(label);
+            var result = EditorGUI.IntField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static void LabelField(ref Rect position, GUIContent label)
         {
-            position.height = EditorGUIUtility.singleLineHeight;
+            position.height = GetLabelHeight();
             EditorGUI.LabelField(position, label);
         }
 
         public static int LayerField(ref Rect position, int value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.LayerMask, label);
-            var result = EditorGUI.LayerField(position, label, value);
+            position.height = GetLayerHeight(label);
+            var result = EditorGUI.LayerField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static int MaskField(ref Rect position, int value, string[] options, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.LayerMask, label);
-            var result = EditorGUI.MaskField(position, label, value, options);
+            position.height = GetMaskHeight(label);
+            var result = EditorGUI.MaskField(position, label ?? GUIContent.none, value, options);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static void ObjectField(ref Rect position, Object value, System.Type type, bool allowSceneObjects, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.ObjectReference, label);
-            var result = EditorGUI.ObjectField(position, label, value, type, allowSceneObjects);
+            position.height = GetObjectHeight(label);
+            var result = EditorGUI.ObjectField(position, label ?? GUIContent.none, value, type, allowSceneObjects);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
         }
 
@@ -153,81 +259,72 @@ namespace CommonEditor
 
         public static Rect RectField(ref Rect position, Rect value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Rect, label);
-            var result = EditorGUI.RectField(position, label, value);
+            position.height = GetRectHeight(label);
+            var result = EditorGUI.RectField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static RectInt RectIntField(ref Rect position, RectInt value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.RectInt, label);
-            var result = EditorGUI.RectIntField(position, label, value);
+            position.height = GetRectIntHeight(label);
+            var result = EditorGUI.RectIntField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
-        public static string TextArea(ref Rect position, string value, float width, GUIStyle style = null)
+        public static string TextArea(ref Rect position, string value, GUIStyle style = null)
         {
-            style ??= EditorStyles.textArea;
-            position.height = style.CalcHeight(new GUIContent(value), width);
-            var result = EditorGUI.TextArea(position, value, style);
+            position.height = GetTextAreaHeight(new GUIContent(value), position.width, style);
+            var result = EditorGUI.TextArea(position, value, style ?? EditorStyles.textArea);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static string TextField(ref Rect position, string value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.String, label);
-            var result = EditorGUI.TextField(position, label, value);
+            position.height = GetTextHeight(label);
+            var result = EditorGUI.TextField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static Vector2 Vector2Field(ref Rect position, Vector2 value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector2, label);
-            var result = EditorGUI.Vector2Field(position, label, value);
+            position.height = GetVector2Height(label);
+            var result = EditorGUI.Vector2Field(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static Vector2Int Vector2IntField(ref Rect position, Vector2Int value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector2Int, label);
-            var result = EditorGUI.Vector2IntField(position, label, value);
+            position.height = GetVector2IntHeight(label);
+            var result = EditorGUI.Vector2IntField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static Vector3 Vector3Field(ref Rect position, Vector3 value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector3, label);
-            var result = EditorGUI.Vector3Field(position, label, value);
+            position.height = GetVector3Height(label);
+            var result = EditorGUI.Vector3Field(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static Vector3Int Vector3IntField(ref Rect position, Vector3Int value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector3Int, label);
-            var result = EditorGUI.Vector3IntField(position, label, value);
+            position.height = GetVector3IntHeight(label);
+            var result = EditorGUI.Vector3IntField(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
 
         public static Vector4 Vector4Field(ref Rect position, Vector4 value, GUIContent label = null)
         {
-            label ??= GUIContent.none;
-            position.height = EditorGUI.GetPropertyHeight(SerializedPropertyType.Vector4, label);
-            var result = EditorGUI.Vector4Field(position, label, value);
+            position.height = GetVector4Height(label);
+            var result = EditorGUI.Vector4Field(position, label ?? GUIContent.none, value);
             position.y += position.height + UEditorGUIUtility.SpaceHeight;
             return result;
         }
