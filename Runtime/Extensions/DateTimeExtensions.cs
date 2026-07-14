bea04f219916b0a037f9551a21ccfec41e208e0c
Sławomir Kmiecik <slawekkmiecik@gmail.com>
2026-07-14 18:05:59 +0200

    Moved timestamp calculations to UTime

--- Runtime/Extensions/DateTimeExtensions.cs
+++ Runtime/Extensions/DateTimeExtensions.cs
@@ -4,43 +4,11 @@ namespace Common
 {
     public static class DateTimeExtensions
     {
-        private const int EpochWeekDayShift = 3;
-
         private static readonly DateTime Epoch = new DateTime(1970, 1, 1);
 
         public static int ToTimestamp(this DateTime self)
         {
             return (int)(self.Subtract(Epoch)).TotalSeconds;
-        }
-
-        public static int ToDayStart(this DateTime self)
-        {
-            return (self.ToTimestamp() / UTime.DaySeconds) * UTime.DaySeconds;
-        }
-
-        public static int ToDayEnd(this DateTime self)
-        {
-            return self.ToDayStart() + UTime.DaySeconds;
-        }
-
-        public static int ToWeekStart(this DateTime self)
-        {
-            return (self.ToWeekIndex() * UTime.WeekDays - EpochWeekDayShift) * UTime.DaySeconds;
-        }
-
-        public static int ToWeekEnd(this DateTime self)
-        {
-            return self.ToWeekStart() + UTime.WeekSeconds;
-        }
-
-        public static int ToDayIndex(this DateTime self)
-        {
-            return ((self.ToTimestamp() / UTime.DaySeconds) % UTime.WeekDays + EpochWeekDayShift) % UTime.WeekDays;
-        }
-
-        public static int ToWeekIndex(this DateTime self)
-        {
-            return ((self.ToTimestamp() / UTime.DaySeconds) + EpochWeekDayShift) / UTime.WeekDays;
         }
     }
 }
--- Runtime/Utilities/UnityClasses/UTime.cs
+++ Runtime/Utilities/UnityClasses/UTime.cs
@@ -18,10 +18,42 @@ namespace Common
 
         public const int WeekHours = WeekDays * DayHours;
 
+        private const int EpochWeekDayShift = 3;
+
         public static float previousTime
             => Time.time - Time.deltaTime;
 
         public static float previousRealtime
             => Time.realtimeSinceStartup - Time.unscaledDeltaTime;
+
+        public static int ToDayStart(int timestamp)
+        {
+            return (timestamp / DaySeconds) * DaySeconds;
+        }
+
+        public static int ToDayEnd(int timestamp)
+        {
+            return ToDayStart(timestamp) + DaySeconds;
+        }
+
+        public static int ToDayIndex(int timestamp)
+        {
+            return ((timestamp / DaySeconds) % WeekDays + EpochWeekDayShift) % WeekDays;
+        }
+
+        public static int ToWeekStart(int timestamp)
+        {
+            return (ToWeekIndex(timestamp) * WeekDays - EpochWeekDayShift) * DaySeconds;
+        }
+
+        public static int ToWeekEnd(int timestamp)
+        {
+            return ToWeekStart(timestamp) + WeekSeconds;
+        }
+
+        public static int ToWeekIndex(int timestamp)
+        {
+            return ((timestamp / DaySeconds) + EpochWeekDayShift) / WeekDays;
+        }
     }
 }
